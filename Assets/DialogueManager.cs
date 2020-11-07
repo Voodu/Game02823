d4166ffb19b56291a62897cc4fbd8dee7f6ca4a8
Piotr Ładoński <piolado@gmail.com>
2020-11-07 19:57:29 +0100

    Add PoC dialogue

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using Ink.Runtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : Singleton<DialogueManager>
+{
+    [SerializeField]
+    private Dialogue currentDialogue;
+
+    [SerializeField]
+    private Canvas canvas;
+
+    [SerializeField]
+    private float elementPadding;
+
+    /* UI Prefabs */
+    [SerializeField]
+    private Text text;
+
+    [SerializeField]
+    private Button button;
+
+    private float offset;
+
+    private void ResetCanvas()
+    {
+        var childCount = canvas.transform.childCount;
+        for (var i = childCount - 1; i >= 0; --i)
+        {
+            Destroy(canvas.transform.GetChild(i).gameObject);
+        }
+
+        offset = 0;
+    }
+
+    public void UpdateDialogueCanvas(List<string> storyLines, List<Choice> choices)
+    {
+        ResetCanvas();
+        UpdateStory(storyLines);
+        UpdateChoices(choices);
+    }
+
+    public void UpdateStory(List<string> storyLines)
+    {
+        foreach (var storyLine in storyLines)
+        {
+            var storyText = Instantiate(text);
+            storyText.text = storyLine;
+            storyText.transform.SetParent(canvas.transform, false);
+            storyText.transform.Translate(new Vector2(0, offset));
+            offset -= storyText.fontSize + elementPadding;
+        }
+    }
+
+    public void UpdateChoices(List<Choice> choices)
+    {
+        foreach (var choice in choices)
+        {
+            var choiceButton = Instantiate(button);
+            choiceButton.transform.SetParent(canvas.transform, false);
+            choiceButton.transform.Translate(new Vector2(0, offset));
+
+            var choiceText = choiceButton.GetComponentInChildren<Text>();
+            choiceText.text = choice.text;
+
+            var layoutGroup = choiceButton.GetComponent<HorizontalLayoutGroup>();
+
+            choiceButton.onClick.AddListener(() => currentDialogue.ChoiceSelected(choice.index));
+
+            offset -= choiceText.fontSize + layoutGroup.padding.top + layoutGroup.padding.bottom + elementPadding;
+        }
+    }
+}
---
+++ Assets/InkStories/Dialogue.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Ink.Runtime;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset inkAsset;
+
+    private Story inkStory;
+    private bool  storyNeeded;
+
+    private void Awake()
+    {
+        inkStory = new Story(inkAsset.text);
+    }
+
+    private void OnEnable()
+    {
+        storyNeeded = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (storyNeeded)
+        {
+            var storyLines = GetNewStoryLines();
+            DialogueManager.Instance.UpdateDialogueCanvas(storyLines, inkStory.currentChoices);
+            storyNeeded = false;
+        }
+    }
+
+    private List<string> GetNewStoryLines()
+    {
+        var storyLines = new List<string>();
+        while (inkStory.canContinue)
+        {
+            storyLines.Add(inkStory.Continue());
+        }
+
+        return storyLines;
+    }
+
+    public void ChoiceSelected(int id)
+    {
+        inkStory.ChooseChoiceIndex(id);
+        storyNeeded = true;
+    }
+}
