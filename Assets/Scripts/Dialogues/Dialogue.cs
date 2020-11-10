35958f4693e41230ae95e63cdfaa1a2a2db6d428
Piotr Ładoński <piolado@gmail.com>
2020-11-10 23:35:10 +0100

    Minor cleanup

---
+++ Assets/Scripts/Dialogues/Dialogue.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using Ink.Runtime;
+using UnityEngine;
+
+namespace Dialogues {
+    public class Dialogue : MonoBehaviour
+    {
+        [SerializeField]
+        private TextAsset inkAsset;
+
+        private Story inkStory;
+
+        public bool         StoryNeeded { get; set; }
+        public List<string> StoryLines  { get; private set; } = new List<string>();
+        public List<string> Tags        { get; private set; } = new List<string>();
+        public List<Choice> Choices     { get; private set; } = new List<Choice>();
+
+        private void Awake()
+        {
+            inkStory = new Story(inkAsset.text);
+        }
+
+        private void OnEnable()
+        {
+            StoryNeeded = true;
+            DialogueManager.Instance.ShowDialogue(this);
+        }
+
+        private void OnDisable()
+        {
+            StoryNeeded = false;
+        }
+
+        private void Update()
+        {
+            if (StoryNeeded)
+            {
+                StoryLines = new List<string>();
+                Tags       = new List<string>();
+
+                while (StoryNeeded && inkStory.canContinue)
+                {
+                    inkStory.Continue();
+                    ProcessNextLine();
+                }
+
+                if (!StoryNeeded)
+                {
+                    return;
+                }
+
+                Choices = inkStory.currentChoices;
+                DialogueManager.Instance.UpdateDialogueCanvas(StoryLines, Choices);
+                StoryNeeded = false;
+            }
+        }
+
+        private void ProcessNextLine()
+        {
+            // Process story
+            StoryLines.Add(inkStory.currentText);
+            // Process tags
+            Tags.AddRange(inkStory.currentTags);
+            // Tags have to be parsed instantly, as they may have special effects on the display
+            DialogueManager.Instance.ParseTags(inkStory.currentTags);
+        }
+
+        public void ChooseSelected(int id)
+        {
+            inkStory.ChooseChoiceIndex(id);
+            StoryNeeded = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogues/DialogueManager.cs
@@ -0,0 +1,105 @@
+using System.Collections.Generic;
+using System.Linq;
+using Ink.Runtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dialogues
+{
+    public class DialogueManager : Singleton<DialogueManager>
+    {
+        [SerializeField]
+        private string playerPrefix = "ME: ";
+
+        private Dialogue dialogue;
+
+        /* UI Prefabs */
+        [SerializeField]
+        private GameObject dialoguePanel;
+
+        [SerializeField]
+        private TextMeshProUGUI npcText;
+
+        [SerializeField]
+        private Button choiceButtonPrefab;
+
+        [SerializeField]
+        private GameObject choiceMenu;
+
+        public void ShowDialogue(Dialogue dialogue)
+        {
+            this.dialogue = dialogue;
+            dialoguePanel.gameObject.SetActive(true);
+        }
+
+        public void DisableDialogue()
+        {
+            dialogue.enabled = false;
+            dialoguePanel.gameObject.SetActive(false);
+        }
+
+        public void UpdateDialogueCanvas(IEnumerable<string> storyLines, IEnumerable<Choice> choices)
+        {
+            ShowStory(storyLines);
+            ClearChoiceMenu();
+            ShowChoices(choices);
+        }
+
+        public void ParseTags(IEnumerable<string> tags)
+        {
+            foreach (var inkTag in tags)
+            {
+                if (inkTag == "terminate")
+                {
+                    EndConversation();
+                }
+            }
+        }
+
+        private void ShowStory(IEnumerable<string> storyLines)
+        {
+            var npcLines = storyLines.Where(storyLine => !storyLine.StartsWith(playerPrefix));
+            var npcStory = string.Join(string.Empty, npcLines);
+            npcText.SetText(npcStory);
+        }
+
+        private void ShowChoices(IEnumerable<Choice> choices)
+        {
+            foreach (var choice in choices)
+            {
+                var choiceButton = Instantiate(choiceButtonPrefab, choiceMenu.transform);
+                choiceButton.onClick.AddListener(() => dialogue.ChooseSelected(choice.index));
+
+                var choiceText = choiceButton.GetComponent<TextMeshProUGUI>();
+                choiceText.SetText(choice.text.Substring(playerPrefix.Length));
+            }
+        }
+
+        private void EndConversation()
+        {
+            dialogue.StoryNeeded = false;
+            ShowStory(dialogue.StoryLines);
+            ClearChoiceMenu();
+            ShowEndingButton();
+        }
+
+        private void ClearChoiceMenu()
+        {
+            var choiceCount = choiceMenu.transform.childCount;
+            for (var i = choiceCount - 1; i >= 0; --i)
+            {
+                Destroy(choiceMenu.transform.GetChild(i).gameObject);
+            }
+        }
+
+        private void ShowEndingButton()
+        {
+            var endButton = Instantiate(choiceButtonPrefab, choiceMenu.transform);
+            endButton.onClick.AddListener(DisableDialogue);
+
+            var endText = endButton.GetComponentInChildren<TextMeshProUGUI>();
+            endText.SetText("[End conversation]");
+        }
+    }
+}
