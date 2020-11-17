b2c993e08b57f95a9b2720da04ed9bca90607c87
Piotr Ładoński <piolado@gmail.com>
2020-11-17 13:59:44 +0100

    Add code & objects testing the basic quest system

---
+++ Assets/Scripts/QuestTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Dialogues;
+using Ink.Runtime;
+using Quests;
+using Quests.Items;
+using UnityEngine;
+
+public class QuestTest : MonoBehaviour
+{
+    public TalkingNPC npc1;
+    public TalkingNPC npc2;
+    public Quest quest;
+
+    public ObjectiveItem item;
+
+    private void Awake()
+    {
+        quest = GetComponent<Quest>();
+        item = GetComponent<ObjectiveItem>();
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (quest != null)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                QuestManager.Instance.Begin(quest.Id);
+            }
+        }
+
+        if (item != null)
+        {
+            if (item.ConnectedObjectiveId == "kill_bear" && Input.GetKeyDown(KeyCode.K))
+            {
+                item.Activate();
+            }
+
+            if (item.ConnectedObjectiveId == "visit_cave" && Input.GetKeyDown(KeyCode.C))
+            {
+                item.Activate();
+            }
+
+            if (item.ConnectedObjectiveId == "inform_man" && Input.GetKeyDown(KeyCode.I))
+            {
+                item.Activate();
+            }
+        }
+    }
+
+    public void Print(string message)
+    {
+        print(message);
+    }
+}
