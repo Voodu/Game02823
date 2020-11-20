6ae88052b3d22365bc71fa8b253afa770a130e3a
Piotr Ładoński <piolado@gmail.com>
2020-11-20 17:14:17 +0100

    Apply various R# suggestions

--- Assets/External Assets/Hero Knight - Pixel Art/Demo/HeroKnight.cs
+++ Assets/External Assets/Hero Knight - Pixel Art/Demo/HeroKnight.cs
@@ -1,106 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
 
-public class HeroKnight : MonoBehaviour {
+public class HeroKnight : MonoBehaviour
+{
+    private static readonly int HurtHash      = Animator.StringToHash("Hurt");
+    private static readonly int DeathHash     = Animator.StringToHash("Death");
+    private static readonly int IdleBlockHash = Animator.StringToHash("IdleBlock");
+    private static readonly int BlockHash     = Animator.StringToHash("Block");
+    private static readonly int RollHash      = Animator.StringToHash("Roll");
+    private static readonly int GroundedHash  = Animator.StringToHash("Grounded");
+    private static readonly int JumpHash      = Animator.StringToHash("Jump");
+    private static readonly int AnimStateHash = Animator.StringToHash("AnimState");
+    private static readonly int AirSpeedYHash = Animator.StringToHash("AirSpeedY");
 
-    [SerializeField] float      m_speed = 4.0f;
-    [SerializeField] float      m_jumpForce = 7.5f;
-    [SerializeField] float      m_rollForce = 6.0f;
-    [SerializeField] bool       m_noBlood = false;
-    [SerializeField] GameObject heart1;
-    [SerializeField] GameObject heart2;
-    [SerializeField] GameObject attackHitBox;
+    [SerializeField]
+    private List<string> items = new List<string>();
 
-    private Animator            m_animator;
-    private Rigidbody2D         m_body2d;
-    private Sensor_HeroKnight   m_groundSensor;
-    private Collider2D          m_enemy;
-    private bool                m_grounded = false;
-    private bool                m_rolling = false;
-    private int                 m_facingDirection = 1;
-    private int                 m_currentAttack = 0;
-    private float               m_timeSinceAttack = 0.0f;
-    private float               m_delayToIdle = 0.0f;
+    [SerializeField]
+    private float speed = 4.0f;
 
-    private string items = "";
-    private int coins = 0;
-    private int lives = 2;
-    private int xp = 0;
-    private bool alive = true;
-    private bool isAttacking = false;
+    [SerializeField]
+    private float jumpForce = 7.5f;
+
+    [SerializeField]
+    private float rollForce = 6.0f;
+
+    [SerializeField]
+    private GameObject heart1;
+
+    [SerializeField]
+    private GameObject heart2;
+
+    [SerializeField]
+    private GameObject attackHitBox;
+
+    private Animator          animator;
+    private Rigidbody2D       body2d;
+    private Sensor_HeroKnight groundSensor;
+    private bool              grounded;
+    private bool              rolling;
+    private int               facingDirection = 1;
+    private int               currentAttack;
+    private float             timeSinceAttack;
+    private float             delayToIdle;
+    private int               coins;
+    private int               lives = 2;
+    private bool              alive = true;
+    private bool              isAttacking;
 
     // Use this for initialization
-    void Start ()
+    private void Start()
     {
-        m_animator = GetComponent<Animator>();
-        m_body2d = GetComponent<Rigidbody2D>();
-        m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_HeroKnight>();
+        animator     = GetComponent<Animator>();
+        body2d       = GetComponent<Rigidbody2D>();
+        groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_HeroKnight>();
         attackHitBox = GameObject.Find("AttackHitbox");
         attackHitBox.SetActive(false);
         heart1 = GameObject.Find("Heart1");
         heart2 = GameObject.Find("Heart2");
     }
 
-
-
     // Update is called once per frame
-    void Update ()
+    private void Update()
     {
-        if (alive == true)
+        if (alive)
         {
             // Increase timer that controls attack combo
-            m_timeSinceAttack += Time.deltaTime;
+            timeSinceAttack += Time.deltaTime;
 
             //Check if character just landed on the ground
-            if (!m_grounded && m_groundSensor.State())
+            if (!grounded && groundSensor.State())
             {
-                m_grounded = true;
-                m_animator.SetBool("Grounded", m_grounded);
+                grounded = true;
+                animator.SetBool(GroundedHash, grounded);
             }
 
             //Check if character just started falling
-            if (m_grounded && !m_groundSensor.State())
+            if (grounded && !groundSensor.State())
             {
-                m_grounded = false;
-                m_animator.SetBool("Grounded", m_grounded);
+                grounded = false;
+                animator.SetBool(GroundedHash, grounded);
             }
 
             // -- Handle input and movement --
-            float inputX = Input.GetAxis("Horizontal");
+            var inputX = Input.GetAxis("Horizontal");
 
             // Swap direction of sprite depending on walk direction
             if (inputX > 0)
             {
                 GetComponent<SpriteRenderer>().flipX = false;
-                m_facingDirection = 1;
+                facingDirection                      = 1;
             }
-                
+
             else if (inputX < 0)
             {
                 GetComponent<SpriteRenderer>().flipX = true;
-                m_facingDirection = -1;
+                facingDirection                      = -1;
             }
 
             // Move
-            if (!m_rolling )
-                m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+            if (!rolling)
+            {
+                body2d.velocity = new Vector2(inputX * speed, body2d.velocity.y);
+            }
 
             //Set AirSpeed in animator
-            m_animator.SetFloat("AirSpeedY", m_body2d.velocity.y);
+            animator.SetFloat(AirSpeedYHash, body2d.velocity.y);
 
-
-            //Death
-            // if (Input.GetKeyDown("e"))
-            // {
-            //     m_animator.SetBool("noBlood", m_noBlood);
-            //     m_animator.SetTrigger("Death");
-            // }
-                
-            //Hurt
-            // else if (Input.GetKeyDown("q"))
-            //     m_animator.SetTrigger("Hurt");
-
-            if (Input.GetKeyDown(KeyCode.Q) && (m_timeSinceAttack > 0.25f))
+            if (Input.GetKeyDown(KeyCode.Q) && (timeSinceAttack > 0.25f))
             {
                 Attack();
             }
@@ -112,11 +120,11 @@ public class HeroKnight : MonoBehaviour {
             {
                 IdleBlock();
             }
-            else if (Input.GetKeyDown(KeyCode.LeftShift) && !m_rolling)
+            else if (Input.GetKeyDown(KeyCode.LeftShift) && !rolling)
             {
                 Roll();
             }
-            else if (Input.GetKeyDown(KeyCode.Space) && m_grounded)
+            else if (Input.GetKeyDown(KeyCode.Space) && grounded)
             {
                 Jump();
             }
@@ -134,70 +142,70 @@ public class HeroKnight : MonoBehaviour {
     private void Idle()
     {
         // Prevents flickering transitions to idle
-        m_delayToIdle -= Time.deltaTime;
-        if (m_delayToIdle < 0)
+        delayToIdle -= Time.deltaTime;
+        if (delayToIdle < 0)
         {
-            m_animator.SetInteger("AnimState", 0);
+            animator.SetInteger(AnimStateHash, 0);
         }
     }
 
     private void Run()
     {
         // Reset timer
-        m_delayToIdle = 0.05f;
-        m_animator.SetInteger("AnimState", 1);
+        delayToIdle = 0.05f;
+        animator.SetInteger(AnimStateHash, 1);
     }
 
     private void Jump()
     {
-        m_animator.SetTrigger("Jump");
-        m_grounded = false;
-        m_animator.SetBool("Grounded", m_grounded);
-        m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
-        m_groundSensor.Disable(0.2f);
+        animator.SetTrigger(JumpHash);
+        grounded = false;
+        animator.SetBool(GroundedHash, grounded);
+        body2d.velocity = new Vector2(body2d.velocity.x, jumpForce);
+        groundSensor.Disable(0.2f);
     }
 
     private void Roll()
     {
-        m_rolling = true;
-        m_animator.SetTrigger("Roll");
-        m_body2d.velocity = new Vector2(m_facingDirection * m_rollForce, m_body2d.velocity.y);
+        rolling = true;
+        animator.SetTrigger(RollHash);
+        body2d.velocity = new Vector2(facingDirection * rollForce, body2d.velocity.y);
     }
 
     private void IdleBlock()
     {
-        m_animator.SetBool("IdleBlock", false);
+        animator.SetBool(IdleBlockHash, false);
     }
 
     private void Block()
     {
-        m_animator.SetTrigger("Block");
-        m_animator.SetBool("IdleBlock", true);
+        animator.SetTrigger(BlockHash);
+        animator.SetBool(IdleBlockHash, true);
     }
 
     private void Attack()
     {
         isAttacking = true;
         StartCoroutine(DoAttack());
-        m_currentAttack++;
+        currentAttack++;
 
         // Loop back to one after third attack
-        if (m_currentAttack > 3)
+        if (currentAttack > 3)
         {
-            m_currentAttack = 1;
+            currentAttack = 1;
         }
 
         // Reset Attack combo if time since last attack is too large
-        if (m_timeSinceAttack > 1.0f)
+        if (timeSinceAttack > 1.0f)
         {
-            m_currentAttack = 1;
+            currentAttack = 1;
         }
 
         // Call one of three attack animations "Attack1", "Attack2", "Attack3"
-        m_animator.SetTrigger("Attack" + m_currentAttack);
+        animator.SetTrigger("Attack" + currentAttack);
 
         // Reset timer
-        m_timeSinceAttack = 0.0f;
+        timeSinceAttack = 0.0f;
     }
 
     private IEnumerator DoAttack()
@@ -229,14 +237,14 @@ public class HeroKnight : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Will we have something like "End" in RPG?
-        if (collision.tag == "End")
+        if (collision.CompareTag("End"))
         {
             Destroy(collision.gameObject);
-            m_body2d.velocity =  new Vector2(0, 0);
+            body2d.velocity = new Vector2(0, 0);
             StatisticsManager.Instance.AddExperience(5);
-            alive             =  false;
-            m_animator.SetTrigger("Block");
-            m_animator.SetBool("IdleBlock", true);
+            alive = false;
+            animator.SetTrigger(BlockHash);
+            animator.SetBool(IdleBlockHash, true);
         }
     }
 
@@ -246,22 +254,22 @@ public class HeroKnight : MonoBehaviour {
 
         if (lives == 0)
         {
-            m_animator.SetTrigger("Death");
+            animator.SetTrigger(DeathHash);
             alive = false;
             Destroy(heart1.gameObject);
-            m_body2d.velocity = new Vector2(0, 0);
+            body2d.velocity = new Vector2(0, 0);
         }
         else
         {
-            m_animator.SetTrigger("Hurt");
+            animator.SetTrigger(HurtHash);
             Destroy(heart2.gameObject);
         }
     }
 
     // Animation Events
     // Called in end of roll animation.
-    void AE_ResetRoll()
+    private void AE_ResetRoll()
     {
-        m_rolling = false;
+        rolling = false;
     }
 }
