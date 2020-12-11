70d9fa95d3cbac0a041850b035e79e50a0349845
Mustafa Sidiqi <mustafasidiqi95@gmail.com>
2020-12-11 22:14:02 +0100

    Added more pickup items

---
+++ Assets/Scripts/Inventory/ItemUse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemUse : MonoBehaviour
+{
+    private GameObject player;
+    //private Controller playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        //playerController = player.GetComponent<Controller>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Use()
+    {
+        Destroy(gameObject);
+        //playerController.TopScore += 20;
+        //playerController.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 1500f);
+    }
+}
